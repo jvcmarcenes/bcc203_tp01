ca51618e9e594deb2b1f94b8a932c9fe05412af1
Panetoni <88630444+Panetoni@users.noreply.github.com>
2023-01-30 21:58:18 -0300

    Add files via upload

---
+++ Acesso.h
@@ -0,0 +1,19 @@
+#ifndef Acesso_h
+#define Acesso_h
+
+#include "gerador.h"
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+#define ITENSPAGINA 4
+#define MAXTABELA 100
+
+
+typedef struct {
+    int chave;
+    int posicao;
+}Indice;
+
